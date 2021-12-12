ae1ba3c5077b4a04e4b5c446598c10ca59abbe47
DannyLeGames <71291074+DannyLeGames@users.noreply.github.com>
2021-12-12 13:24:05 -0500

    Added falling physics of tetrominos using clock system from SFML. Also added references.

--- Tetris/Main.cpp
+++ Tetris/Main.cpp
@@ -1,8 +1,21 @@
+// Installing SFML took a lot of time. References used to set up SFML are:
+// https://www.youtube.com/watch?v=neIoDQ71yb0 and
+// https://www.youtube.com/watch?v=YfMQyOw1zik
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
+// I used a couple of video references to make this game. 
+// First reference is from javidx9: https://www.youtube.com/watch?v=8OK8_tHeCIA
+// I learned a lot about the fundamentals of Tetris from him such as some of the algorithm to consider.
+// Linked to first reference github is: https://github.com/OneLoneCoder/videos/blob/master/OneLoneCoder_Tetris.cpp
+
+// Second reference is from Kofybrek: https://www.youtube.com/watch?v=vkS1fY_UTyg
+// The link to the github is: https://github.com/Kofybrek/Tetris
+// Helped me through with understanding how SFML works and how to make use of the library. It served as a base guidance when creating the game.
+
 using namespace sf;
 
+// Board size. M is height, N is width.
 const int M = 20;
 const int N = 10;
 
@@ -57,19 +70,32 @@ int main() {
 
 	//Reference for learning how to include music is from:
 	//https://www.youtube.com/watch?v=fm2Hi7082d0
-	//Make sure to use audacity to convert mp3 files to wav/ogg files because mp3 is not supported in SFML due to legal issues.
+	//Make sure to use Audacity to convert mp3 files to wav/ogg files because mp3 is not supported in SFML due to legal issues.
 	//Simple set up procedure. This will play the song immediately when the program starts.
 	//Music music;
 	//music.openFromFile("audio/WillowTree.ogg");
 	//music.setVolume(50);
 	//music.play();
 
+	//Variables set up for rotate, x position which is dx, and colour identification for later.
 	int dx = 0; 
 	bool rotate = false; 
 	int colourNum = 1;
 
+	//Clock set up to add in the falling physic. Physic will be base off of computer clock.
+	float timer = 0;
+	float delay = 0.3;
+
+	Clock clock;
+
 	while (window.isOpen())
 	{
+		//Initializing clock and adding it to timer which is used to run the falling physics.
+		//Because I am adding time into the timer, I need to reset clock as time elapsed so the timer doesn't accelerate due to accumulation of time past.
+		float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+		timer += time;
+
 		Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == Event::Closed) 
@@ -101,7 +127,14 @@ int main() {
 			}
 		}
 
+		// Falling physics. y co-ordinates is increased everytime it falls.
+		if (timer > delay) {
+			for (int i = 0; i < 4; i++)
+				a[i].y += 1;
+			timer = 0;
+		}
 
+		// n is the list of tetrominos from 0 to 6, order is I, Z, S, T, L, J, O (these are tetris pieces)
 		int n = 3;
 		if (a[0].x == 0)
 			for (int i = 0; i < 4; i++) {
